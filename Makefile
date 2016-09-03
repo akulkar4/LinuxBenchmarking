b4e1500fbc669d50e1112f8c3c5e9fea0bac383f
Nishad Sabnis <nsabnis@ncsu.edu>
2016-09-02 20:36:15 -0400

    Added thread creation overhead calculation

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 CC=gcc
-CFLAGS=-I .
+CFLAGS=-lpthread 
 
 all: main.o meas_ovhd.o proc_ovhd.o syscall_ovhd.o creation_ovhd.o
-	$(CC) -o benchmark main.o meas_ovhd.o proc_ovhd.o syscall_ovhd.o creation_ovhd.o -I .
+	$(CC) -o benchmark main.o meas_ovhd.o proc_ovhd.o syscall_ovhd.o creation_ovhd.o -lpthread
 
 clean:
 	rm -rf *.o *.c~ *.h~ benchmark Makefile~
--- creation_ovhd.c
+++ creation_ovhd.c
@@ -7,7 +7,18 @@
 #include<pthread.h>
 #include<sys/wait.h>
 
+static void process_overhead(void);
+static void thread_overhead(void);
+static void *random_fn(void *ptr);
+
 void creation_overhead(void)
+{
+  process_overhead();
+  thread_overhead();
+}
+
+
+static void process_overhead(void)
 {
   uint64_t start, end, pid;
   int status;
@@ -28,4 +39,20 @@ void creation_overhead(void)
     }
 }
 
-  
+static void thread_overhead(void)
+{
+  uint64_t start, end;
+  void *ptr;
+
+  pthread_t thread_one;
+  start = meas_start();
+  pthread_create(&thread_one,NULL,random_fn,ptr); 
+  pthread_join(thread_one,NULL);
+  end = meas_stop();
+  printf("Thread creation overhead:%lu\n", end-start);
+}
+
+static void *random_fn(void *ptr)
+{
+  return NULL;
+}
