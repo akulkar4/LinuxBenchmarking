249721459cd0ec45b65c62ede8a3754feb17502c
Nishad Sabnis <nsabnis@ncsu.edu>
2016-09-15 16:07:08 -0400

    getpid() changed to getppid(), Atom whitespace changes

--- syscall_ovhd.c
+++ syscall_ovhd.c
@@ -1,4 +1,4 @@
-#include"syscall.h"
+
 #include"rdtsc.h"
 #include<stdio.h>
 #include<stdint.h>
@@ -9,12 +9,7 @@ void system_call_overhead(void)
 {
   uint64_t start, end;
   float count_ns;
-  int cpu;
-  
-  start = meas_start();
-  //getpid();
-  syscall(SYS_getcpu, &cpu, NULL, NULL);
+  getppid();
   end = meas_stop();
   count_ns = meas_convert_to_us(end - start);
-  printf("System call(getpid) overhead is: %.3f ns\n",count_ns);
 }
