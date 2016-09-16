03b739ac09596ca5a0de20aab26494a31391f09f
Alok Pradip Kulkarni <akulkar4@ncsu.edu>
2016-09-15 22:56:48 -0400

    Pipe overhead consideration added in context switch measurement

--- cswitch_ovhd.c
+++ cswitch_ovhd.c
@@ -11,18 +11,34 @@ uint64_t start, end;
 void context_switch_overhead(void)
 {
   int pipefd[2];
+  int testPipe[2];
   pid_t pid;
   size_t ret;
   int result;
-  
+  uint64_t testStart, testEnd, pipeOvhd;
+
+  //create two pipes
   result = pipe(pipefd);
   if (result < 0)
     {
-      //failure in creating a pipe
       perror("pipe");
       exit (1);
     }
-  
+  result = pipe(testPipe);
+  if (result < 0)
+  {
+    perror("testPipe");
+    exit (1);
+  }
+
+  //measuring testPipe read-write latency before forking
+  testStart = meas_start();
+  ret = write(testPipe[1], &testStart, sizeof(testStart));
+  ret = read(testPipe[0], &testStart, sizeof(testStart));
+  testEnd = meas_stop();
+  pipeOvhd = testStart - testEnd;
+  printf("Pipe call overhead: %lu\n",pipeOvhd);
+
   pid = fork();
   if (pid == 0)
     {
@@ -40,7 +56,7 @@ void context_switch_overhead(void)
       ret = read (pipefd[0], &start, sizeof(start));
       //sampling rdtscp after context switch
       end = meas_stop();
-      count_ns = meas_convert_to_us(end-start);
+      count_ns = meas_convert_to_us(end-start-pipeOvhd);
       printf("Context switch overhead: %.3f ns\n",count_ns);
     }
 }
